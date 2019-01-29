13aa1edfaf128478b3dcc30552f9029d6e23789e
Giuseppe Battista <giusedroid@gmail.com>
2019-01-29 17:14:29 +0000

    [EDIT] example linting

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,46 +1,47 @@
 const endpoints = {
-    notes:[
-        {
-            noteId: 0,
-            content: "A Sample Note\nHere is a second line",
-            createdAt: 1548152070949,
-            attachment: true
-        },
-        
-        {
-            noteId: 1,
-            content: "This is just a mock\nHere is a second line",
-            createdAt: 1548152070999
-        },
-        
-        {
-            noteId: 2,
-            content: "You realize it, right?\nHere is a second line",
-            createdAt: 1548152072534,
-            attachment: true
-        }
+  notes: [
+    {
+      noteId: 0,
+      content: 'A Sample Note\nHere is a second line',
+      createdAt: 1548152070949,
+      attachment: true,
+    },
 
-    ]
-}
+    {
+      noteId: 1,
+      content: 'This is just a mock\nHere is a second line',
+      createdAt: 1548152070999,
+    },
+
+    {
+      noteId: 2,
+      content: 'You realize it, right?\nHere is a second line',
+      createdAt: 1548152072534,
+      attachment: true,
+    },
+
+  ],
+};
 
 export default class API {
-    static post(name, path, options){
-        return Promise.resolve({
-            name,
-            path,
-            options
-        });
-    }
+  static post(name, path, options) {
+    return Promise.resolve({
+      name,
+      path,
+      options,
+    });
+  }
 
-    static get(name, path){
-        return endpoints[name];
-    }
+  static get(name) {
+    return endpoints[name];
+  }
 
-    static getItem(id){
-        return endpoints['notes'].filter( note => note.noteId === parseInt(id))[0] || endpoints['notes'][0];
-    }
+  static getItem(id) {
+    return endpoints
+      .notes.filter(note => note.noteId === parseInt(id, 10))[0] || endpoints.notes[0];
+  }
 
-    static getAttachment(){
-        return "https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg"
-    }
+  static getAttachment() {
+    return 'https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg';
+  }
 }
